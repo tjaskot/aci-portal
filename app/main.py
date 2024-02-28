34b0bd15f7e2f4a6c3399a0f6c9f039e34ba7696
trevor jaskot <trevord186@gmail.com>
2024-02-27 21:03:28 -0600

    major framework and front end updates

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ from typing import Annotated
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 security = HTTPBasic()
 templates = Jinja2Templates(directory="templates")
 
@@ -46,17 +47,29 @@ def get_current_username(
 
 @app.get("/")
 async def root(request: Request):
+    """
+    Description.
+    :return:
+    """
     return RedirectResponse(url="/login")
 
 
 @app.get("/users/admin")
 def read_current_user(username: Annotated[str, Depends(get_current_username)]):
+    """
+    Description.
+    :return:
+    """
     return {"username": username}
 
 
 # Request details from Starlette: https://www.starlette.io/requests/
 @app.api_route("/login", methods=["GET", "POST"])
 async def login(request: Request):
+    """
+    Description.
+    :return:
+    """
     if request.method == "POST":
         # print(request.method)
         # async with request.form() as form:
@@ -72,6 +85,10 @@ async def login(request: Request):
 
 @app.get("/index", response_class=HTMLResponse)
 async def home(request: Request):
+    """
+    Description.
+    :return:
+    """
     if request.method.lower() != 'get':
         # Raise http exceptions for unsupported request methods, Json responses for data handling
         raise HTTPException(
@@ -83,20 +100,28 @@ async def home(request: Request):
         return templates.TemplateResponse("index.html", {"request": request, "index_var": index_var})
 
 
-@app.get("/second")
-async def second(request: Request):
-    second_var = 'Second_variable'
-    return templates.TemplateResponse("second.html", {"request": request, "second_var": second_var})
+@app.get("/search")
+async def search(request: Request):
+    """
+    Description.
+    :return:
+    """
+    search_var = 'Search_variable'
+    return templates.TemplateResponse("search.html", {"request": request, "search_var": search_var})
 
 
 @app.get("/api")
 async def default(request: Request):
+    """
+    Description.
+    :return:
+    """
     return JSONResponse(
         content={
             "Info": "Below these are the endpoints that are reserved for our application with a brief description",
             "/": "Root page for SSO",
             "index": "Index Url - html response",
-            "second": "Second browser endpoint - html response",
+            "search": "Second browser endpoint - html response",
             "keepalive": "Keepalive 200 response",
             "api": "List of api endpoints, both browser and json api responses - Json Response",
             "docs": "Swagger API docs"
@@ -107,6 +132,10 @@ async def default(request: Request):
 
 @app.get("/keepalive")
 async def keepalive():
+    """
+    Description.
+    :return:
+    """
     return {
         "keepalive":
             200
