95eee0657bee273a4196db6c92e28f77fb704f20
trevor jaskot <trevord186@gmail.com>
2024-02-27 22:15:05 -0600

    updating visuals and requests for pages completed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 # Fast API Docs : https://fastapi.tiangolo.com/
 # import uvicorn  # When using debugging, uncomment debug if statement at bottom of file.
 import secrets
+import requests
+import smtplib
+from starlette.requests import Request as StarletteRequest  # To be used with Request.body forms
 from fastapi import FastAPI, Request, HTTPException, Form, Depends, HTTPException, status
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials, OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -9,6 +12,9 @@ from fastapi.templating import Jinja2Templates
 from dotenv import dotenv_values, load_dotenv
 from pathlib import Path
 from typing import Annotated
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 
 app = FastAPI()
@@ -98,6 +104,77 @@ async def home(request: Request):
     else:
         index_var = 'Index_variable'
         return templates.TemplateResponse("index.html", {"request": request, "index_var": index_var})
+
+
+@app.get("/readData")
+async def search(request: Request):
+    """
+    Description.
+    :return:
+    """
+    return templates.TemplateResponse("read_customer_data.html", {"request": request})
+
+
+@app.get("/changeCrm")
+async def search(request: Request):
+    """
+    Description.
+    :return:
+    """
+    return templates.TemplateResponse("change_crm.html", {"request": request})
+
+
+@app.get("/invoices")
+async def search(request: Request):
+    """
+    Description.
+    :return:
+    """
+    # For mimetype, need to use requests package, not fastapi Request
+    # TODO: get Request.body from form working in fastapi
+    # r = requests.get("")
+    # print(r)
+    # # TODO: Mimetype e-mail
+    # requestor = request.form['siteOwner']
+    # # Change the string into list for concatenation later in function
+    # recipient = [requestor]
+    # cc = ['email@email.com']
+    # bcc = []
+    # sender = 'email@email.com'
+    # msg = MIMEMultipart('alternative')
+    # msg['Subject'] = 'Request Submitted'
+    # msg['From'] = sender
+    # msg['To'] = ",".join(recipient)
+    # msg['CC'] = ",".join(cc)
+    # msg['BCC'] = ",".join(bcc)
+    # recipient += cc + bcc
+    # # More detail in e-mail content
+    # email_body = None
+    # email_body_header = ' '
+    # email_body_header += '<html><head></head><body>'
+    # email_body_header += '<style type="text/css"></style>'
+    # email_body_header += '<br><h2>Hello</h2><p>Email Header</p><br><p>Requested Info:</p><br>'
+    # email_body_content = ' '
+    # email_body_content += '<p> + requestor + </p>'
+    # email_body_footer = ' '
+    # email_body_footer += '<br>Thank you.'
+    # email_body_footer += '<br><p>R/</p><p>Stalker Radar</p><br>'
+    # html = str(email_body_header) + str(email_body_content) + str(email_body_footer)
+    # part = MIMEText(html, 'html')
+    # msg.attach(part)
+    # s = smtplib.SMTP('mailhost.stalkerradar.com')
+    # s.sendmail(sender, recipient, msg.as_string())
+    # s.quit()
+    return templates.TemplateResponse("list_pay_invoices.html", {"request": request})
+
+
+@app.get("/askForHelp")
+async def search(request: Request):
+    """
+    Description.
+    :return:
+    """
+    return templates.TemplateResponse("ask_for_help.html", {"request": request})
 
 
 @app.get("/search")
